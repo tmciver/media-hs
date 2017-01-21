55e7fe25b4e7861e6407e5a3ededbb7a66a1ade8
Tim McIver <tim@timmciver.com>
2017-01-20 21:03:56 -0500

    Fixed shadowed `id` and updated test description.

--- test/HSpecTests.hs
+++ test/HSpecTests.hs
@@ -6,12 +6,12 @@ import Domain.Media
 main :: IO ()
 main = hspec $ do
 
-  describe "Test 'addMediaHandler'" $ do
+  describe "Test `mediaHandler` for `AddMedia` command." $ do
     it "should return a list containing a single event: MediaWasAdded" $ do
       events <- mediaHandler command
-      events `shouldBe` [MediaWasAdded id path mediaClass]
+      events `shouldBe` [MediaWasAdded id' path mediaClass]
         where command = AddMedia path
               path = "/home/user/media/photo1.jpg"
-              id = "d6432bddab5d2ef255a1922ee45f85ac89636ec8"
+              id' = "d6432bddab5d2ef255a1922ee45f85ac89636ec8"
               mediaClass = Photo
               
